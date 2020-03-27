122a2035ad75b793fd95a3be5e55dbc0b46aa82b
damiel <daniel@users.noreply.github.com>
2020-03-27 17:56:16 +0800

    添加security进行权限配置

---
+++ src/main/java/org/javaboy/actuator/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package org.javaboy.actuator.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SecurityConfig
+ * 权限配置
+ * @author arctic
+ * @date 2020/3/27
+ **/
+@Configuration
+public class SecurityConfig {
+}
