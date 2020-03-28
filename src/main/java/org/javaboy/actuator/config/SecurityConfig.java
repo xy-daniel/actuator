f778d9a4113fbf7f583cdcd16bd91ac2579c45c7
p4m4p <myarctic@163.com>
2020-03-28 16:38:42 +0800

    去掉security作为可视化界面的客户端

--- src/main/java/org/javaboy/actuator/config/SecurityConfig.java
+++ src/main/java/org/javaboy/actuator/config/SecurityConfig.java
@@ -1,26 +1,26 @@
-package org.javaboy.actuator.config;
-
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/**
- * SecurityConfig
- * 权限配置
- * @author arctic
- * @date 2020/3/27
- **/
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.requestMatcher(EndpointRequest.toAnyEndpoint())
-                .authorizeRequests()
-                .anyRequest().hasRole("admin")
-                .and()
-                .httpBasic();
-    }
-
-}
+//package org.javaboy.actuator.config;
+//
+//import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///**
+// * SecurityConfig
+// * 权限配置
+// * @author arctic
+// * @date 2020/3/27
+// **/
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.requestMatcher(EndpointRequest.toAnyEndpoint())
+//                .authorizeRequests()
+//                .anyRequest().hasRole("admin")
+//                .and()
+//                .httpBasic();
+//    }
+//
+//}
